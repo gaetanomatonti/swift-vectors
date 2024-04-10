0677b4caefd93b61f8cb6a478219510fa7bd7e41
gaetanomatonti <gaetanomatonti@icloud.com>
2024-04-10 16:31:02 +0200

    feat!: add vector protocol
    Replace the type alias with a protocol that types can conform to.

--- Tests/VectorsTests/VectorsTests.swift
+++ Tests/VectorsTests/VectorsTests.swift
@@ -3,68 +3,68 @@ import XCTest
 
 final class VectorsTests: XCTestCase {
   func testSum() {
-    let a = Vector(x: 1, y: 2)
-    let b = Vector(x: 3, y: 4)
-    XCTAssertEqual(a + b, Vector(x: 4, y: 6))
+    let a = CGPoint(x: 1, y: 2)
+    let b = CGPoint(x: 3, y: 4)
+    XCTAssertEqual(a + b, CGPoint(x: 4, y: 6))
   }
 
   func testSubtraction() {
-    let a = Vector(x: 1, y: 2)
-    let b = Vector(x: 3, y: 4)
+    let a = CGPoint(x: 1, y: 2)
+    let b = CGPoint(x: 3, y: 4)
 
-    XCTAssertEqual(b - a, Vector(x: 2, y: 2))
+    XCTAssertEqual(b - a, CGPoint(x: 2, y: 2))
   }
 
   func testDirectionOfSubtraction() {
-    let a = Vector(x: 1, y: 2)
-    let b = Vector(x: 3, y: 4)
+    let a = CGPoint(x: 1, y: 2)
+    let b = CGPoint(x: 3, y: 4)
 
     XCTAssertEqual((b - a).heading, .degrees(45))
     XCTAssertEqual((a - b).heading, .degrees(-135))
   }
 
   func testScalarMultiplication() {
-    let a = Vector(x: 1, y: 2)
+    let a = CGPoint(x: 1, y: 2)
 
-    XCTAssertEqual(a * 2, Vector(x: 2, y: 4))
+    XCTAssertEqual(a * 2, CGPoint(x: 2, y: 4))
   }
 
   func testScalarDivision() {
-    let a = Vector(x: 4, y: 6)
+    let a = CGPoint(x: 4, y: 6)
 
-    XCTAssertEqual(a / 2, Vector(x: 2, y: 3))
+    XCTAssertEqual(a / 2, CGPoint(x: 2, y: 3))
   }
 
   func testMagnitude() {
-    let a = Vector(x: 3, y: 4)
-    let b = Vector(x: 0, y: 0)
+    let a = CGPoint(x: 3, y: 4)
+    let b = CGPoint(x: 0, y: 0)
 
     XCTAssertEqual(a.magnitude, 5)
     XCTAssertEqual(b.magnitude, 0)
   }
 
   func testHeading() {
-    let a = Vector(x: 4, y: -4)
+    let a = CGPoint(x: 4, y: -4)
 
     XCTAssertEqual(a.heading.degrees, -45)
   }
 
   func testNormalization() {
-    let a = Vector(x: 3, y: 4)
-    let b = Vector(x: 0.5, y: 0.5)
-    let c = Vector(x: 0, y: 0)
+    let a = CGPoint(x: 3, y: 4)
+    let b = CGPoint(x: 0.5, y: 0.5)
+    let c = CGPoint(x: 0, y: 0)
 
     XCTAssertEqual(a.normalized.magnitude, 1, accuracy: 0.001)
     XCTAssertEqual(b.normalized.magnitude, 1, accuracy: 0.001)
 
-    XCTAssertEqual(c.normalized, Vector(x: 0, y: 0))
+    XCTAssertEqual(c.normalized, CGPoint(x: 0, y: 0))
   }
 
   func testLimit() {
-    let a = Vector(x: 6, y: 8)
-    let b = Vector(x: 3, y: 4)
+    let a = CGPoint(x: 6, y: 8)
+    let b = CGPoint(x: 3, y: 4)
 
-    XCTAssertEqual(a.limit(5), Vector(x: 3, y: 4))
-    XCTAssertEqual(b.limit(6), Vector(x: 3, y: 4))
+    XCTAssertEqual(a.limit(5), CGPoint(x: 3, y: 4))
+    XCTAssertEqual(b.limit(6), CGPoint(x: 3, y: 4))
   }
 }
