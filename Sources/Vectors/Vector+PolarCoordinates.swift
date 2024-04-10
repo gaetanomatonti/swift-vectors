c1f381318facfc0c8875bb8c7c19e20d197dc5bd
gaetanomatonti <gaetanomatonti@icloud.com>
2024-04-10 19:37:38 +0200

    feat: remove init with rect

--- Sources/Vectors/Vector+PolarCoordinates.swift
+++ Sources/Vectors/Vector+PolarCoordinates.swift
@@ -17,22 +17,4 @@ public extension Vector {
       y: sin(angle.radians) * radius + center.y
     )
   }
-
-  /// Creates an instance of a ``Vector`` in the coordinate system of the specified ``CGRect``.
-  ///
-  /// - Parameters:
-  ///   - angle: The angular coordinate of the vector.
-  ///   - percentage: A value in the range `[0, 1]` used to scale the radius of the `rect`.
-  ///                 Values below `0.0` are interpreted as `0.0` and values above `1.0` are interpreted as `1.0`.
-  ///   - rect: The rectangle defining the coordinate system of the vector, used to compute the maximum value of the radius.
-  init(angle: Angle, percentage: CGFloat = 1, in rect: CGRect) {
-    let percentage = max(0, min(percentage, 1))
-    let smallestSide = min(rect.width, rect.height)
-    let radius = smallestSide / 2
-    let scaledRadius = percentage * radius
-    let offset = min(rect.midX, rect.midY)
-    let center = CGPoint(x: offset, y: offset)
-
-    self.init(angle: angle, radius: scaledRadius, center: center)
-  }
 }
