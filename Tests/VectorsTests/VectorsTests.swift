8398407525a6f1e0713f78173b80476473e9d606
gaetanomatonti <gaetanomatonti.dev@gmail.com>
2025-02-27 21:48:31 +0100

    feat: improve tests

--- Tests/VectorsTests/VectorsTests.swift
+++ Tests/VectorsTests/VectorsTests.swift
@@ -3,20 +3,38 @@ import Testing
 @testable import Vectors
 
 @Suite("Vector Operations Tests") struct VectorOperationsTests {
-  @Test func testSum() {
+  @Test func sum() {
     let a = CGPoint(x: 1, y: 2)
     let b = CGPoint(x: 3, y: 4)
     #expect(a + b == CGPoint(x: 4, y: 6))
   }
+  
+  @Test func mutableSum() {
+    var a = CGPoint(x: 1, y: 2)
+    let b = CGPoint(x: 3, y: 4)
+    
+    a += b
+    
+    #expect(a == CGPoint(x: 4, y: 6))
+  }
 
-  @Test func testSubtraction() {
+  @Test func subtraction() {
     let a = CGPoint(x: 1, y: 2)
     let b = CGPoint(x: 3, y: 4)
 
     #expect(b - a == CGPoint(x: 2, y: 2))
   }
 
-  @Test func testDirectionOfSubtraction() {
+  @Test func mutableSubtraction() {
+    let a = CGPoint(x: 1, y: 2)
+    var b = CGPoint(x: 3, y: 4)
+
+    b -= a
+    
+    #expect(b == CGPoint(x: 2, y: 2))
+  }
+  
+  @Test func subtractionHeading() {
     let a = CGPoint(x: 1, y: 2)
     let b = CGPoint(x: 3, y: 4)
 
@@ -24,19 +42,35 @@ import Testing
     #expect((a - b).heading == .degrees(-135))
   }
 
-  @Test func testScalarMultiplication() {
+  @Test func scalarMultiplication() {
     let a = CGPoint(x: 1, y: 2)
 
     #expect(a * 2 == CGPoint(x: 2, y: 4))
   }
+  
+  @Test func mutableScalarMultiplication() {
+    var a = CGPoint(x: 1, y: 2)
+    
+    a *= 2
 
-  @Test func testScalarDivision() {
+    #expect(a == CGPoint(x: 2, y: 4))
+  }
+
+  @Test func scalarDivision() {
     let a = CGPoint(x: 4, y: 6)
 
     #expect(a / 2 == CGPoint(x: 2, y: 3))
   }
 
-  @Test func testMagnitude() {
+  @Test func mutableScalarDivision() {
+    var a = CGPoint(x: 4, y: 6)
+
+    a /= 2
+    
+    #expect(a == CGPoint(x: 2, y: 3))
+  }
+
+  @Test func magnitude() {
     let a = CGPoint(x: 3, y: 4)
     let b = CGPoint(x: 0, y: 0)
 
@@ -44,28 +78,51 @@ import Testing
     #expect(b.magnitude == 0)
   }
 
-  @Test func testHeading() {
+  @Test func heading() {
     let a = CGPoint(x: 4, y: -4)
 
     #expect(a.heading.degrees == -45)
   }
 
-  @Test func testNormalization() {
+  @Test(
+    arguments: [
+      CGPoint(x: 3, y: 4),
+      CGPoint(x: 0.5, y: 0.5)
+    ]
+  )
+  func normalization(vector: Vector) {
     let a = CGPoint(x: 3, y: 4)
     let b = CGPoint(x: 0.5, y: 0.5)
-    let c = CGPoint(x: 0, y: 0)
 
-    #expect(a.normalized.magnitude.isApproximatelyEqual(to: 1, relativeTolerance: 0.001))
-    #expect(b.normalized.magnitude.isApproximatelyEqual(to: 1, relativeTolerance: 0.001))
+    #expect(a.normalized.length.isApproximatelyEqual(to: 1, relativeTolerance: 0.001))
+    #expect(b.normalized.length.isApproximatelyEqual(to: 1, relativeTolerance: 0.001))
+  }
+  
+  @Test func zeroVectorNormalization() {
+    let zero = CGPoint.zero
+    #expect(zero.normalized.length == .zero, "The length of a zero vector should always be zero.")
+  }
+  
+  @Test(
+    arguments: [
+      CGPoint(x: 3, y: 4),
+      CGPoint(x: 0.5, y: 0.5)
+    ]
+  )
+  func mutableNormalization(vector: Vector) {
+    var vector = vector
+    vector.normalize()
 
-    #expect(c.normalized == CGPoint(x: 0, y: 0))
+    #expect(vector.length.isApproximatelyEqual(to: 1, relativeTolerance: 0.001))
   }
 
-  @Test func testLimit() {
+  @Test func limit() {
     let a = CGPoint(x: 6, y: 8)
     let b = CGPoint(x: 3, y: 4)
+    let c = CGPoint(x: 2, y: 1)
 
-    #expect(a.limit(5) == CGPoint(x: 3, y: 4))
-    #expect(b.limit(6) == CGPoint(x: 3, y: 4))
+    #expect(a.limit(5) == CGPoint(x: 3, y: 4), "The components of the vector should change if its length exceeds the specified limit.")
+    #expect(b.limit(6) == CGPoint(x: 3, y: 4), "The components of the vector should change only if its length exceeds the specified limit.")
+    #expect(c.limit(6) == CGPoint(x: 2, y: 1), "The components of the vector should change only if its length exceeds the specified limit.")
   }
 }
